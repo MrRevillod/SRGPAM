b43158a1306a4a3461d11f6f4b2935298906b80b
SrCarlito <carlitisamuel@gmail.com>
2024-08-30 21:15:18 -0400

    feat: :camera_flash: Creacion de funcionalidades multipart
    Se crea la funcionalidad basica para recibir un archivo y almacenarlo en el servidor (disco)

--- apps/storage/src/index.ts
+++ apps/storage/src/index.ts
@@ -1,9 +1,11 @@
 import cors from "cors"
 import helmet from "helmet"
 import morgan from "morgan"
+import multer from "multer"
 import express, { Router } from "express"
-
-import { log, config, errorHandler, extensions } from "@repo/lib"
+import { existsSync, mkdirSync } from "fs"
+import { log, config, errorHandler, extensions, JsonResponse } from "@repo/lib"
+import path from "path"
 
 export const createServer = (): express.Express => {
 	const app = express()
@@ -20,12 +22,44 @@ export const createServer = (): express.Express => {
 	return app
 }
 
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		const dir = `public/${req.params["id"]}`
+
+		//Crear directorio personal
+		if (!existsSync(dir)) {
+			mkdirSync(dir)
+		}
+		cb(null, "public/" + req.params["id"] + "/")
+	},
+	filename: (req, file, cb) => {
+		const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9)
+		const ext = path.extname(file.originalname)
+		cb(null, file.fieldname + "-" + uniqueSuffix + ext)
+	},
+})
+const upload = multer({ storage })
+
 const router: Router = Router()
-router.post("/api/storage/seniors/:id", async () => {})
-router.delete("/api/storage/seniors/:id", async () => {})
+router.post("/api/storage/seniors/:id", upload.single("file"), (req, res) => {
+	const response: JsonResponse = {
+		message: "Upload sucess",
+		type: "success",
+		values: null,
+	}
+	return res.status(200).json(response)
+})
+
+router.delete("/api/storage/seniors/:id", async (req, res, next) => {
+	const response: JsonResponse = {
+		message: "delete route correct",
+		type: "success",
+		values: null,
+	}
+	return res.status(200).json(response)
+})
 
 const server = createServer()
-
 server.listen(config.api_port, () => {
-	log(`api running on ${config.api_port}`)
+	log(`Storage running on ${config.api_port}`)
 })
