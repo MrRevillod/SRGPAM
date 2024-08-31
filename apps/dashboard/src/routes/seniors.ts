a3a0a04c33f3c920af56d93f70c3b81131eff61e
benjita2002djsjsda <benjamintwo2002@gmail.com>
2024-08-30 21:21:05 -0400

    feat: :art: Configuracion de multer
    Configuracion de multer para recibir archivos en memoria

--- apps/dashboard/src/routes/seniors.ts
+++ apps/dashboard/src/routes/seniors.ts
@@ -1,4 +1,6 @@
 import { Router } from "express"
+import { upload } from "../config"
+import { JsonResponse } from "@repo/lib"
 
 const router: Router = Router()
 
@@ -11,6 +13,17 @@ router.patch("/:id/deny", async (req, res, next) => {})
 router.patch("/:id/validate", async (req, res, next) => {})
 router.post("/mobile", async (req, res, next) => {})
 router.post("/pre-checked", async (req, res, next) => {})
-router.post("/:id/profile-picture", async (req, res, next) => {})
+router.post("/:id/profile-picture", upload.single("image1"), (req, res) => {
+	const file = req.file
+
+	console.log(file)
+	console.log(req.body)
+	const response: JsonResponse = {
+		message: file !== undefined ? "File recived" : "File not recived",
+		type: !file !== undefined ? "success" : "error",
+		values: null,
+	}
+	return res.json(response)
+})
 
 export default router
