891814402a4b9540a9a53972cfdb0fbe9464b1e7
tcurihual <tvcurihual@gmail.com>
2024-09-02 22:35:33 -0400

    feat: :seedling: Verificación hook-form con z en mobile
    se verifico el funcionamiento de react-hook-form con z en mobile previo a la pr

--- apps/mobile/app/register.tsx
+++ apps/mobile/app/register.tsx
@@ -6,7 +6,8 @@ import Colors from "@/components/colors"
 import Input from "@/components/register/input"
 import GeneralView from "@/components/register/generalView"
 import CustomButton from "@/components/register/button"
-import registerSchema from "@/utils/validation"
+import Camera from "@/components/register/camera"
+import { registerSchema } from "@/utils/validation"
 
 const Stack = createNativeStackNavigator()
 
@@ -20,11 +21,9 @@ type commonProps = {
 const RUT = ({ navigation, control, errors }: commonProps) => {
 	return (
 		<GeneralView title="Datos del Registro" textCircle="1/7" textTitle="Ingresa tu RUT">
-			<View>
-				<View>
-					<Input name="rut" placeholder="Ingresa tu RUT" control={control} errors={errors} />
-				</View>
-				<CustomButton title="Siguiente" onPress={() => navigation.navigate("Email")} />
+			<View style={styles.container}>
+				<Input name="rut" placeholder="Ingresa tu RUT" control={control} errors={errors} />
+				<CustomButton title="Siguiente" onPress={() => navigation.navigate("Pin")} />
 			</View>
 		</GeneralView>
 	)
@@ -38,10 +37,8 @@ const Email = ({ navigation, control, errors }: commonProps) => {
 			textTitle="¿Deseas Ingresar Email?"
 			textDescription="Este campo es opcional por lo que usted decice si ingresarlo"
 		>
-			<View>
-				<View>
-					<Input name="Email" placeholder="TuCorreo@gmail.com" control={control} errors={errors} />
-				</View>
+			<View style={styles.container}>
+				<Input name="email" placeholder="TuCorreo@gmail.com" control={control} errors={errors} />
 				<CustomButton title="Siguiente" onPress={() => navigation.navigate("Pin")} />
 				<CustomButton
 					style={{ backgroundColor: Colors.white }}
@@ -57,10 +54,8 @@ const Email = ({ navigation, control, errors }: commonProps) => {
 const Pin = ({ navigation, control, errors }: commonProps) => {
 	return (
 		<GeneralView title="Datos del Registro" textCircle="3/7" textTitle="Ingresa tu Pin de 4 dígitos">
-			<View>
-				<View>
-					<Input name="pin" placeholder="Ingresa tu pin" control={control} errors={errors} />
-				</View>
+			<View style={styles.container}>
+				<Input name="pin" placeholder="Ingresa tu pin" control={control} errors={errors} />
 				<CustomButton title="Siguiente" onPress={() => navigation.navigate("ConfirmPin")} />
 				<CustomButton
 					style={{ backgroundColor: Colors.white }}
@@ -76,10 +71,8 @@ const Pin = ({ navigation, control, errors }: commonProps) => {
 const ConfirmPin = ({ navigation, control, errors }: commonProps) => {
 	return (
 		<GeneralView title="Datos del Registro" textCircle="4/7" textTitle="Vuelve a ingresar tu Pin">
-			<View>
-				<View>
-					<Input name="pinConfirm" placeholder="Confirma tu PIN" control={control} errors={errors} secureTextEntry={true} />
-				</View>
+			<View style={styles.container}>
+				<Input name="pinConfirm" placeholder="Confirma tu PIN" control={control} errors={errors} secureTextEntry={true} />
 				<CustomButton title="Siguiente" onPress={() => navigation.navigate("Cedula")} />
 				<CustomButton
 					style={{ backgroundColor: Colors.white }}
@@ -102,7 +95,7 @@ const Cedula = ({ navigation, control, errors }: commonProps) => {
 			textDescription="Recuerda que la imagen debe ser legible y estar actualizada"
 		>
 			<View>
-				<CustomButton style={{ marginTop: 30 }} title="Abrir Cámara" onPress={() => navigation.navigate("RSH")} />
+				<CustomButton style={{ marginTop: 30 }} title="Abrir Cámara" onPress={() => navigation.navigate("Camera")} />
 				<CustomButton
 					style={{ backgroundColor: Colors.white }}
 					textStyle={styles.customButtonText}
@@ -150,7 +143,12 @@ const Final = ({ navigation }: commonProps) => {
 			textTitle="Todo Listo!"
 			textDescription="Has completado el registro. Cuando la municipalidad valide tus datos seras notificado y podras solicitar horas"
 		>
-			<></>
+			<CustomButton
+				style={{ backgroundColor: Colors.white }}
+				textStyle={styles.customButtonText}
+				title="Volver"
+				onPress={() => navigation.goBack()}
+			/>
 		</GeneralView>
 	)
 }
@@ -165,6 +163,7 @@ const FormNavigator = ({ control, handleSubmit, errors }: { control: any; handle
 			<Stack.Screen name="Cedula">{(props) => <Cedula {...props} control={control} errors={errors} />}</Stack.Screen>
 			<Stack.Screen name="RSH">{(props) => <RSH {...props} control={control} errors={errors} handleSubmit={handleSubmit} />}</Stack.Screen>
 			<Stack.Screen name="Final">{(props) => <Final {...props} />}</Stack.Screen>
+			<Stack.Screen name="Camera" component={Camera} />
 		</Stack.Navigator>
 	)
 }
@@ -189,6 +188,12 @@ const App = () => {
 export default App
 
 const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		flexDirection: "column",
+		justifyContent: "center",
+	},
+
 	inputError: {
 		borderColor: "red",
 	},
--- apps/mobile/utils/validation.ts
+++ apps/mobile/utils/validation.ts
@@ -1,9 +1,8 @@
 import * as z from "zod"
 
-const registerSchema = z.object({
+export const registerSchema = z.object({
 	rut: z.string().min(1, "RUT es requerido"),
+	email: z.string().optional(),
 	pin: z.string().min(1, "PIN es requerido"),
 	pinConfirm: z.string().min(1, "Confirmación de PIN es requerida"),
 })
-
-export default registerSchema
