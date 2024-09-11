1e08673adcb9f3b215368c32409f94ac48f3f8fd
SrCarlito <carlitisamuel@gmail.com>
2024-09-10 21:19:39 -0300

    feat: :safety_vest: Valicacion Archivos
    Se crean los middlewares para validación de mimetypes y tamaños de archivos

--- apps/dashboard/src/routes/seniors.ts
+++ apps/dashboard/src/routes/seniors.ts
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import { upload, seniorsRegisterMobileImages } from "../config"
 import { registerSeniorFromMobile, handleSeniorRequest } from "../controllers/seniors"
+import { fileValidation } from "../middlewares/file"
 
 const router: Router = Router()
 
@@ -9,7 +10,7 @@ router.get("/:id", async (req, res, next) => {})
 router.patch("/:id", async (req, res, next) => {})
 router.delete("/:id", async (req, res, next) => {})
 router.get("/new", async (req, res, next) => {})
-router.post("/new-mobile", seniorsRegisterMobileImages, registerSeniorFromMobile)
+router.post("/new-mobile", seniorsRegisterMobileImages, fileValidation,registerSeniorFromMobile)
 router.post("/pre-checked", async (req, res, next) => {})
 router.patch("/:id/new", handleSeniorRequest)
 router.post("/:id/profile-picture", upload.single("image1"), (req, res) => {})
