1608778eed00e25e1354b4dfeba559da1bb058b1
benjitauwu <benjamintwo2002@gmail.com>
2024-09-22 19:43:59 -0300

    feat: :art: Modificaciones diseños y input de mi perfil
    Se agregaron los input de cambiar contraseña y mejoras de diseño

--- apps/admin-web/src/pages/Profile.tsx
+++ apps/admin-web/src/pages/Profile.tsx
@@ -1,122 +1,73 @@
-import React, { useState } from "react"
+import React from "react"
 import { Input } from "../components/ui/Input"
 
 const ProfilePage: React.FC = () => {
-	const [activeTab, setActiveTab] = useState("account")
-
-	const handleTabChange = (tab: string) => {
-		setActiveTab(tab)
-	}
-
 	return (
-		<div
-			className="flex flex-col lg:flex-row min-h-screen p-8 space-y-6 lg:space-y-0 lg:space-x-8"
-			style={{
-				background: "linear-gradient(135deg, #c3ec52 0%, #0ba29d 100%)",
-			}}
-		>
-			<aside className="lg:w-1/4 bg-white shadow-xl rounded-2xl p-6 flex flex-col items-center space-y-4 transition-transform transform hover:scale-105">
-				<div className="relative">
-					<img
-						src="https://img.wattpad.com/3b476bee1010537b280291a2351c9db47cb6cc69/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f59396c356e432d435970715148773d3d2d313032333434363734362e313636316631643432363766316136353731343532373739353538382e6a7067?s=fit&w=720&h=720"
-						alt="User Profile"
-						className="rounded-full w-32 h-32 object-cover border-4 border-white"
-					/>
-					<button className="absolute right-0 bottom-0 bg-blue-500 text-white p-2 rounded-full border-2 border-white shadow-md">
-						<i className="fas fa-camera"></i>
-					</button>
-				</div>
-
-				<div className="text-center">
-					<h2 className="text-xl font-semibold">Benjamin Luis Ignacio Espinoza Parra</h2>
-					<p className="text-gray-500">UCT</p>
-				</div>
-
-				<div className="w-full text-left space-y-2">
-					<div className="flex justify-between">
-						<span className="font-medium">Opportunities applied</span>
-						<span className="text-orange-500 font-bold">32</span>
+		<div className="flex w-full login-container items-center justify-center absolute">
+			<div className="bg-white flex flex-col justify-center items-center px-12 w-11/12 md:w-2/3 lg:w-2/5 xl:w-1/3 2xl:w-1/3 rounded-lg h-auto py-6">
+				<div className="w-full flex flex-col items-center space-y-4 my-6">
+					<div className="relative">
+						<img
+							src="https://img.wattpad.com/3b476bee1010537b280291a2351c9db47cb6cc69/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f59396c356e432d435970715148773d3d2d313032333434363734362e313636316631643432363766316136353731343532373739353538382e6a7067?s=fit&w=720&h=720"
+							alt="User Profile"
+							className="rounded-full w-32 h-32 object-cover border-4 border-white"
+						/>
+						<button className="absolute right-0 bottom-0 bg-green-800 text-white p-2 rounded-full border-2 border-white shadow-md">
+							<i className="fas fa-camera"></i>
+						</button>
 					</div>
-					<div className="flex justify-between">
-						<span className="font-medium">Opportunities won</span>
-						<span className="text-green-500 font-bold">26</span>
-					</div>
-					<div className="flex justify-between">
-						<span className="font-medium">Current opportunities</span>
-						<span className="text-blue-500 font-bold">6</span>
+
+					<div className="text-center">
+						<h2 className="text-4xl font-bold text-gray-900 mb-2">Benjamin Luis Ignacio Espinoza Parra</h2>
+						<p className="text-gray-600">Rol</p>
 					</div>
 				</div>
 
-				<button className="mt-4 bg-blue-500 text-white rounded-lg py-2 px-4 w-full hover:bg-blue-600 transition-colors">
-					Ver Perfil
-				</button>
-				<p className="text-sm text-gray-500 mt-2">https://domain.com/user</p>
-			</aside>
+				<div className="w-full">
+					<h2 className="text-2xl font-bold text-gray-900 mb-4">Configuración de cuenta</h2>
+					<form className="space-y-4">
+						<div className="grid grid-cols-2 gap-4">
+							<Input
+								label="Nombre completo"
+								name="firstName"
+								type="text"
+								placeholder="Benjamin Luis Ignacio Espinoza Parra"
+							/>
+							<Input label="Correo Electrónico" name="email" type="email" placeholder="bluis@alu.com" />
+						</div>
 
-			<div className="lg:w-3/4 bg-white shadow-xl rounded-2xl p-8 flex-grow transition-transform transform hover:scale-105">
-				<div className="border-b border-gray-200 mb-6">
-					<ul className="flex space-x-4">
-						<li>
-							<button
-								className={`py-2 px-4 ${activeTab === "account" ? "border-b-2 border-blue-500 text-blue-500" : "text-gray-500"}`}
-								onClick={() => handleTabChange("account")}
-							>
-								Configuración de cuenta
-							</button>
-						</li>
-						<li>
-							<button
-								className={`py-2 px-4 ${activeTab === "notifications" ? "border-b-2 border-blue-500 text-blue-500" : "text-gray-500"}`}
-								onClick={() => handleTabChange("notifications")}
-							>
-								Notificaciones
-							</button>
-						</li>
-					</ul>
-				</div>
+						<div className="grid grid-cols-1 gap-4">
+							<Input label="Rut" name="rut" type="text" placeholder="EJ:12.345.678-9" />
+						</div>
 
-				{activeTab === "account" && (
-					<div>
-						<h2 className="text-xl font-semibold mb-6">Configuración de cuenta</h2>
-						<form className="space-y-4">
-							<div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+						<div className="mt-6">
+							<h3 className="text-xl font-semibold text-gray-800 mb-2">Cambiar Contraseña</h3>
+							<div className="grid grid-cols-2 gap-4">
 								<Input
-									label="Nombre completo"
-									name="firstName"
-									type="text"
-									defaultValue="Benjamin Luis Ignacio Espinoza Parra"
+									label="Nueva contraseña"
+									name="newPassword"
+									type="password"
+									placeholder="Ingrese su nueva contraseña"
+								/>
+								<Input
+									label="Confirmar nueva contraseña"
+									name="confirmNewPassword"
+									type="password"
+									placeholder="Confirme su nueva contraseña"
 								/>
 							</div>
-							<div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-								<Input
-									label="Correo Electronico"
-									name="email"
-									type="email"
-									defaultValue="bluis@alu.com"
-								/>
-							</div>
-							<div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-								<Input label="Rut" name="rut" type="text" defaultValue="21.190.399-6" />
-							</div>
+						</div>
 
-							<div className="mt-4">
-								<button
-									type="submit"
-									className="bg-blue-500 text-white rounded-lg py-2 px-4 w-full lg:w-auto hover:bg-blue-600 transition-colors"
-								>
-									Actualizar
-								</button>
-							</div>
-						</form>
-					</div>
-				)}
-
-				{activeTab === "notifications" && (
-					<div>
-						<h2 className="text-xl font-semibold mb-6">Notificaciones</h2>
-						<p>No hay notificaciones.</p>
-					</div>
-				)}
+						<div className="mt-4">
+							<button
+								type="submit"
+								className="bg-green-800 text-neutral-100 rounded-lg py-2 px-4 w-full lg:w-auto hover:bg-green-900 transition-colors"
+							>
+								Actualizar
+							</button>
+						</div>
+					</form>
+				</div>
 			</div>
 		</div>
 	)
