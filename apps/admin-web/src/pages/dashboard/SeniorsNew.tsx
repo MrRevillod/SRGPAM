5c764cc3dccbd8c97e3627d4d484134fb456de46
CristobalSg <111024562+CristobalSg@users.noreply.github.com>
2024-09-23 06:09:16 -0300

    fix: :zap: Se migro NewSeniors y se optimizo codigo, como tambien se implemento bien la logica de verificar
    Se definio utilizar AntD para el componente tabla de forma definitiva, se mudo la tabla newSeniors y se optimizo el codigo utilizando los componentes creados Table y Modals con anterioridad. Se corrigio la logica de validar aplicando la logica de base de datos, (validate=true o validate=false). Se corrigio el mapeo de los datos, ahora va directamente la informacion de el backend a el front, la tabla.

--- apps/admin-web/src/pages/dashboard/SeniorsNew.tsx
+++ apps/admin-web/src/pages/dashboard/SeniorsNew.tsx
@@ -1,37 +1,24 @@
 import axios from "axios"
 import React, { useState, useEffect } from "react"
-import { Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow, Modal, Button } from "flowbite-react"
-
-interface Senior {
-	id: string
-	name: string
-	email: string
-	birthDate: string
-	address: string
-	validated: boolean
-}
+import PageLayout from "../../layouts/PageLayout"
+import Modals from "../../components/Modal"
+import PersonTable from "../../components/Table"
+import { DataType } from "../../lib/types"
+import { Button } from "antd"
+import { columnsConfig } from "../../lib/table-columns"
 
 const NewSeniorsPage: React.FC = () => {
-	const [data, setData] = useState<Senior[]>([]) // Estado para los datos de seniors
-	const [loading, setLoading] = useState(true) // Estado de carga
-	const [error, setError] = useState<string | null>(null) // Estado para el error
-	const [selectedSenior, setSelectedSenior] = useState<Senior | null>(null) // Senior seleccionado
-	const [isModalOpen, setIsModalOpen] = useState(false) // Estado para controlar la apertura del modal
+	const [data, setData] = useState<DataType[]>([])
+	const [loading, setLoading] = useState(true)
+	const [selectedSenior, setSelectedSenior] = useState<DataType | null>(null)
+	const [isModalOpen, setIsModalOpen] = useState(false)
 
 	const fetchSeniors = async () => {
 		try {
 			const response = await axios.get("http://localhost/api/dashboard/seniors/new")
-			const transformedData = response.data.values.seniors.map((senior: any) => ({
-				id: senior.id,
-				name: senior.name,
-				email: senior.email,
-				birthDate: senior.birthDate,
-				address: senior.address,
-				validated: senior.validated,
-			}))
-			setData(transformedData)
+			setData(response.data.values.seniors)
 		} catch (err) {
-			setError("Error al obtener los datos de seniors")
+			console.error("Error al obtener los datos de seniors")
 		} finally {
 			setLoading(false)
 		}
@@ -41,7 +28,26 @@ const NewSeniorsPage: React.FC = () => {
 		fetchSeniors()
 	}, [])
 
-	const handleOpenModal = (senior: Senior) => {
+	const handleValidate = async (validate: boolean) => {
+		if (selectedSenior && selectedSenior.id) {
+			try {
+				const url = `http://localhost/api/dashboard/seniors/${selectedSenior.id}/new?validate=${validate}`
+				await axios.patch(url)
+
+				setData((prevData) =>
+					prevData.map((senior) =>
+						senior.id === selectedSenior.id ? { ...senior, validated: validate } : senior
+					)
+				)
+
+				handleCloseModal()
+			} catch (error) {
+				console.error("Error al actualizar el usuario:", error)
+			}
+		}
+	}
+
+	const handleOpenModal = (senior: DataType) => {
 		setSelectedSenior(senior)
 		setIsModalOpen(true)
 	}
@@ -51,120 +57,55 @@ const NewSeniorsPage: React.FC = () => {
 		setSelectedSenior(null)
 	}
 
-	const handleUpdate = async () => {
-		if (selectedSenior && selectedSenior.id) {
-			try {
-				const url = `http://localhost/api/dashboard/seniors/${selectedSenior.id}/new?validated=true`
-				await axios.patch(url)
-
-				setData((prevData) =>
-					prevData.map((senior) =>
-						senior.id === selectedSenior.id ? { ...senior, validated: true } : senior,
-					),
-				)
-				handleCloseModal()
-			} catch (error) {
-				console.error("Error al actualizar el usuario:", error)
-			}
-		}
-	}
-
-	const handleDelete = async () => {
-		if (selectedSenior && selectedSenior.id) {
-			try {
-				await axios.delete(`http://localhost:5000/api/dashboard/seniors/${selectedSenior.id}`)
-				setData((prevData) => prevData.filter((senior) => senior.id !== selectedSenior.id))
-				handleCloseModal()
-			} catch (error) {
-				console.error("Error al eliminar el usuario:", error)
-			}
-		}
-	}
-
 	return (
-		<div className="overflow-x-auto">
-			<Table hoverable>
-				<TableHead>
-					<TableHeadCell>ID</TableHeadCell>
-					<TableHeadCell>Name</TableHeadCell>
-					<TableHeadCell>Email</TableHeadCell>
-					<TableHeadCell>Address</TableHeadCell>
-					<TableHeadCell>Birth Date</TableHeadCell>
-					<TableHeadCell>Validated</TableHeadCell>
-					<TableHeadCell>
-						<span className="sr-only">Edit</span>
-					</TableHeadCell>
-				</TableHead>
-				<TableBody className="divide-y">
-					{Array.isArray(data) ? (
-						data.map((senior) => (
-							<TableRow key={senior.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
-								<TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-									{senior.id}
-								</TableCell>
-								<TableCell>{senior.name}</TableCell>
-								<TableCell>{senior.email}</TableCell>
-								<TableCell>{senior.address}</TableCell>
-								<TableCell>{new Date(senior.birthDate).toLocaleDateString()}</TableCell>
-								<TableCell>{senior.validated ? "Yes" : "No"}</TableCell>
-								<TableCell>
-									<a
-										href="#"
-										onClick={() => handleOpenModal(senior)}
-										className="text-cyan-600 hover:underline dark:text-cyan-500"
-									>
-										Edit
-									</a>
-								</TableCell>
-							</TableRow>
-						))
-					) : (
-						<TableRow>
-							<TableCell colSpan={7}>No data available</TableCell>
-						</TableRow>
-					)}
-				</TableBody>
-			</Table>
+		<PageLayout pageTitle="Solicitudes de registro nuevo adultos mayores">
+			<PersonTable
+				data={data}
+				columnsConfig={columnsConfig} // Usa el columnsConfig importado
+				onEdit={handleOpenModal}
+			/>
 
-			{/* Modal para editar los detalles del senior */}
-			<Modal dismissible show={isModalOpen} onClose={handleCloseModal}>
-				<Modal.Header>Edit Senior Details</Modal.Header>
-				<Modal.Body>
-					{selectedSenior && (
-						<div className="space-y-6">
-							<p>
-								<strong>Name: </strong>
-								{selectedSenior.name}
-							</p>
-							<p>
-								<strong>Email: </strong>
-								{selectedSenior.email}
-							</p>
-							<p>
-								<strong>Address: </strong>
-								{selectedSenior.address}
-							</p>
-							<p>
-								<strong>Birth Date: </strong>
-								{new Date(selectedSenior.birthDate).toLocaleDateString()}
-							</p>
-							<p>
-								<strong>Validated: </strong>
-								{selectedSenior.validated ? "Yes" : "No"}
-							</p>
-						</div>
-					)}
-				</Modal.Body>
-				<Modal.Footer>
-					<Button color="light" onClick={handleUpdate}>
+			{/* Modal personalizado para editar los detalles del senior */}
+			<Modals
+				title="Edit Senior Details"
+				isVisible={isModalOpen}
+				onOk={() => handleValidate(true)}
+				onCancel={handleCloseModal}
+				footer={[
+					<Button key="update" onClick={() => handleValidate(true)}>
 						Aceptar
-					</Button>
-					<Button color="failure" onClick={handleDelete}>
+					</Button>,
+					<Button key="delete" danger onClick={() => handleValidate(false)}>
 						Denegar
-					</Button>
-				</Modal.Footer>
-			</Modal>
-		</div>
+					</Button>,
+				]}
+			>
+				{selectedSenior && (
+					<div>
+						<p>
+							<strong>Nombre: </strong>
+							{selectedSenior.name}
+						</p>
+						<p>
+							<strong>Correo Electrónico: </strong>
+							{selectedSenior.email}
+						</p>
+						<p>
+							<strong>Dirección: </strong>
+							{selectedSenior.address}
+						</p>
+						<p>
+							<strong>Fecha de Nacimiento: </strong>
+							{new Date(selectedSenior.birthDate).toLocaleDateString()}
+						</p>
+						<p>
+							<strong>Verificado: </strong>
+							{selectedSenior.validated ? "Yes" : "No"}
+						</p>
+					</div>
+				)}
+			</Modals>
+		</PageLayout>
 	)
 }
 
