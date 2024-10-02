3f0ed663d78bbbb16d274b5a4085d141317f9650
tcurihual <tvcurihual@gmail.com>
2024-10-01 23:20:18 -0300

    feat: :memo: Inicializacion de middleware de pertenencia y estudio de como se podria estructurar (sujeto a cambios y errores))

--- apps/dashboard/src/middlewares/validation.ts
+++ apps/dashboard/src/middlewares/validation.ts
@@ -36,3 +36,36 @@ export const validateSchema = (schema: SomeZodObject | ZodEffects<ZodObject<any,
 		}
 	}
 }
+
+// Notas @aliwenn: Segun lo que entendi, decidi dividir en dos el middleware, uno para checkear eventos y el otro para checkear usuarios
+// en primera instancia, si la id no existe, se lanza un error de peticion invalida
+// luego, busco la id del usuario con el rol ADMIN con la funcion del @lib, si existe, dejo el admin pasar
+// si esa validacion no se toma en cuenta entonces busco en EVENTOS si la id proporcionada le pertenece a un PROFESIONAL o SENIOR
+// a mi criterio no es necesario validar que tipo de usuario es, ya que solo se me solicita validar pertenencia del evento a un usuario
+// por ende si el usuario esta en el evento, esto quiere decir que le pertenece y por consecuencia puede pasar al siguiente middleware o controller
+// segun el endpoint en el que se encuentre el middleware
+
+// Problemas que se me vienen a la mente:
+// - Que ocurre si la id del Profesional esta relacionada con multiples eventos? si yo solo estoy recibiendo la id del usuario, como se que evento le pertenece?
+// - Si el usuario no tiene eventos relacionados, como se procede? se lanza un error? se le permite pasar?
+
+export const eventOwner = async (req: Request, res: Response, next: NextFunction) => {
+	if (!req.params.id) throw new AppError(400, "Petición inválida")
+	try {
+		if (await findUser({ id: req.params.id }, "ADMIN")) next()
+	} catch (error) {
+		next(error)
+	}
+}
+
+// Para el caso de los usuarios, se procede de la misma manera que con los eventos, se valida si la id existe, si el usuario es ADMIN y
+// si no se procede a buscar en la base de datos,
+
+export const userOwner = async (req: Request, res: Response, next: NextFunction) => {
+	if (!req.params.id) throw new AppError(400, "Petición inválida")
+	try {
+		if (await findUser({ id: req.params.id }, "ADMIN")) next()
+	} catch (error) {
+		next(error)
+	}
+}
